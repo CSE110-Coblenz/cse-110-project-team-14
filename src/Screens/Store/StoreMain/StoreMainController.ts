0bde7634ed355f815e0ed6c7dcb9829638025cf7
atz002 <atz002@ucsd.edu>
2025-12-05 11:42:25 -0800

    added progress tracking feature and comments to account for store items into total progress bar

--- src/Screens/Store/StoreMain/StoreMainController.ts
+++ src/Screens/Store/StoreMain/StoreMainController.ts
@@ -1,16 +1,19 @@
 import type { ScreenSwitcher } from "../../../types";
 import { ScreenController } from "../../../types";
-import { StoreMainModel } from './StoreMainModel';
-import { StoreMainView } from './StoreMainView';
+import { StoreMainModel } from "./StoreMainModel";
+import { StoreMainView } from "./StoreMainView";
+import { ProgressTracker } from "../../../utils/ProgressTracker";
 
 export class StoreMainController extends ScreenController {
   private model: StoreMainModel;
   private view: StoreMainView;
   private screenSwitcher: ScreenSwitcher;
+  private tracker: ProgressTracker;
 
-  constructor(screenSwitcher: ScreenSwitcher) {
+  constructor(screenSwitcher: ScreenSwitcher, tracker: ProgressTracker) {
     super();
     this.screenSwitcher = screenSwitcher;
+    this.tracker = tracker;
 
     this.model = new StoreMainModel();
 
@@ -21,15 +24,19 @@ export class StoreMainController extends ScreenController {
     );
   }
 
-  // Show the store screen and load content 
+  // Show the store screen and load content
   async start(): Promise<void> {
     this.view.loadBackground("Public/Background/store.png");
     await this.model.load_items("/ItemImage/Store/items.json");
     const items = this.model.get_items();
     this.view.showItem(items, (itemName) => this.handleItemClick(itemName));
     this.view.showClerk("ItemImage/Store/cashier.png", 1000, 280, 300, 400);
-    
-    this.view.show();  // Make store screen visible
+
+    // load progress tracker, count items loaded
+    const ids = items.map((item) => `store:${item.name}`);
+    this.tracker.registerItems("storeItems", ids);
+
+    this.view.show(); // Make store screen visible
   }
 
   // Handle item clicked and update dock
@@ -37,6 +44,7 @@ export class StoreMainController extends ScreenController {
     this.model.select_item(itemName);
     const selected = this.model.get_selected_item();
     if (!selected) return;
+    this.tracker.markFound("storeItems", `store:${itemName}`); // tracker marker
     this.view.updateDock(selected);
   }
 
@@ -46,9 +54,8 @@ export class StoreMainController extends ScreenController {
   }
 
   private switchToIntro(): void {
-  this.screenSwitcher.switchToScreen({ type: "Intro" });
+    this.screenSwitcher.switchToScreen({ type: "Intro" });
   }
-
 
   /** Show/hide methods for screen management */
   show(): void {
@@ -62,7 +69,4 @@ export class StoreMainController extends ScreenController {
   getView(): StoreMainView {
     return this.view;
   }
-
 }
-
-
